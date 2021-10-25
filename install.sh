e39586b09e810e1756f24dfb01ba75b8f076f5d5
spoukke <sacha.bernheim@hey.com>
2021-10-25 17:44:49 +0200

    fix: remove package install check

--- install.sh
+++ install.sh
@@ -12,14 +12,6 @@ function _info() {
 # Run script from directory where the script is stored.
 cd "$( dirname "${BASH_SOURCE[0]}" )"
 
-_info "🔎 Looking for packages..."
-PKGS="bspwm sxhkd alacritty"
-for pkg in $PKGS; do
-  which $pkg &>/dev/null
-  [ $? -eq 0 ] || echo "$pkg not found."
-done
-_info "✔️ All packages are installed!"
-
 _info "🔧 Copying config files..."
 cp .xsessionrc ~/.xsessionrc
 cp -r config/bspwm ~/.config
@@ -27,5 +19,3 @@ cp -r config/sxhkd ~/.config/sxhkd
 cp -r config/alacritty ~/.config/alacritty
 
 _info "🚀 You are ready to go!"
-echo "Don't forget to uninstall gnome!"
-
